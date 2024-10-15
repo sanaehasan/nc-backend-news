4c1210fde147a8cdce3d14ae40d12cc11af0dac0
sanae <sanae.k1986@gmail.com>
2024-10-15 18:13:27 +0100

    delete console.log

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -3,7 +3,6 @@ const { fetchComments } = require("../model/commentsModel");
 exports.getCommentsByArticleId = (request, response, next) => {
   return fetchComments(request.params.article_id)
     .then((comments) => {
-      console.log(comments);
       if (comments.length === 0) {
         response.status(200).send({ msg: "Article has no comments yet" });
       }
